18f27919370f162e5e6f7c136c59406ca6edfc5a
Ftoupet <2132122@cmontmorency.qc.ca>
2023-09-27 22:10:00 -0400

    Amelioration formulaire

--- client/src/components/forms/updateProfile/Profil.jsx
+++ client/src/components/forms/updateProfile/Profil.jsx
@@ -5,14 +5,20 @@ import "./css/Profil.css";
 
 function Profil() {
   const { userId } = useContext(UserContext);
-  const [userEmail, setUserEmail] = useState(""); // Pour stocker l'email de l'utilisateur actuel
+  const [userEmail, setUserEmail] = useState("");
   const [updatedEmail, setUpdatedEmail] = useState("");
   const [updatedName, setUpdatedName] = useState("");
-  const URL = "http://127.0.0.1:5001";
+  const [oldName, setOldName] = useState("");
+  const [oldEmail, setOldEmail] = useState("");
+  const URL = "http://127.0.0.1:5001"; // HARDCODE
 
   useEffect(() => {
     getUserInfo(userId);
   }, [userId]);
+
+  useEffect(() => {
+    setUserEmail(updatedEmail);
+  }, [updatedEmail]);
 
   const getUserInfo = async (userId) => {
     try {
@@ -20,6 +26,8 @@ function Profil() {
       const userData = response.data.user;
       setUpdatedName(userData.username);
       setUserEmail(userData.email);
+      setOldEmail(userData.email)
+      setOldName(userData.username)
     } catch (error) {
       console.error(error);
     }
@@ -32,8 +40,14 @@ function Profil() {
       email: updatedEmail,
       id: userId
     };
+    if(updatedUser.email==''){
+      updatedUser.email=oldEmail;
+    }
+    if(updatedUser.username==''){
+      updatedUser.username=oldName;
+    }
+    console.log(updatedUser)
     try {
-      console.log(URL);
       const response = await axios.post(
         URL + "/api/user/update-user",
         updatedUser
@@ -58,21 +72,21 @@ function Profil() {
           type="text"
           id="username"
           name="username"
-          placeholder="Enter your username"
-          value={updatedName}
+          placeholder={updatedName}
+          // value={updatedName}
           onChange={(e) => setUpdatedName(e.target.value)}
         />
 
         <label htmlFor="email">Votre adresse courriel</label>
         <input
-          type="email"
+          type="text"
           id="email"
           name="email"
-          placeholder="Enter your email"
-          value={userEmail}
+          placeholder={userEmail}
+          // value={userEmail} // Utilisez l'email de l'utilisateur actuel
           onChange={(e) => setUpdatedEmail(e.target.value)}
         />
-        <input type="submit" value="Submit" />
+        <input type="submit" value="Appliquer les changements" />
       </form>
     </div>
   );
