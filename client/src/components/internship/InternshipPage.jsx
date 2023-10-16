0f5b68743dcfe540ebf06798aa61e9050b401fcc
SarahMerzouk <sarahmerzouk10@gmail.com>
2023-10-16 14:23:08 -0400

    affichage des informations du stage

--- client/src/components/internship/InternshipPage.jsx
+++ client/src/components/internship/InternshipPage.jsx
@@ -8,7 +8,22 @@ function InternshipPage(){
 
     return(
         <div>
-          <p>{internship.internshiptitle}</p>
+          <h1>{internship.internshiptitle}</h1>
+          <p>{internship.internshipdescription}</p>
+
+          <h4>Personne contact</h4>
+          <p>{internship.contactname}</p>
+          <p>{internship.contactemail}</p>
+          <p>{internship.contactphone}</p>
+
+          <h4>Adresse:</h4>
+          <p>{internship.companyadresse}</p>
+
+          <h4>Informations supplémentaires:</h4>
+          <p>Nombre de positions pour ce stage: {internship.nbpositions}</p>
+          <p>Salaire: {internship.salary} $/h</p>
+          <p>Date de création: {internship.creationdate}</p>
+
         </div>
     );
 }
