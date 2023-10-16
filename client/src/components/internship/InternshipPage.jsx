81fc5e6b025d146e246b3c1bce669adfec378040
SarahMerzouk <sarahmerzouk10@gmail.com>
2023-10-16 14:41:17 -0400

    validation de s'il y a des soumissions ou non

--- client/src/components/internship/InternshipPage.jsx
+++ client/src/components/internship/InternshipPage.jsx
@@ -1,30 +1,39 @@
 import React from "react";
 import { useLocation } from "react-router";
+import "./css/internshipPage.css";
 
 function InternshipPage(){
   const location = useLocation();
   const internship = location.state;
-  console.log(internship);
+  const nbCandidatures = internship.applicantlist.length;
 
-    return(
+  return(
         <div>
           <h1>{internship.internshiptitle}</h1>
           <p>{internship.internshipdescription}</p>
 
-          <h4>Personne contact</h4>
-          <p>{internship.contactname}</p>
-          <p>{internship.contactemail}</p>
-          <p>{internship.contactphone}</p>
+          {nbCandidatures != 0 && (<div>
+            <h3>Liste d'applicants : </h3>
+            <table className="table">
+              <tr>
+                <th>Nom de l'étudiant</th>
+                <th>Courriel de l'étudiant</th>
+                <th>Date de soumission de la candidature</th>
+              </tr>
 
-          <h4>Adresse:</h4>
-          <p>{internship.companyadresse}</p>
+              <tr>
+                <th>test</th>
+                <th>test</th>
+                <th>test</th>
+              </tr>
 
-          <h4>Informations supplémentaires:</h4>
-          <p>Nombre de positions pour ce stage: {internship.nbpositions}</p>
-          <p>Salaire: {internship.salary} $/h</p>
-          <p>Date de création: {internship.creationdate}</p>
+            </table>
+          </div>)}
 
+          {nbCandidatures == 0 && (<div>
+            <p>Aucune soumission</p>
+          </div>)}
         </div>
-    );
+  );
 }
 export default InternshipPage;
