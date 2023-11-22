ea70f20edb4001702a27fea2b6873dfc621966ce
SarahMerzouk <sarahmerzouk10@gmail.com>
2023-11-22 17:25:22 -0500

    bug : aucun stage trouvé quand y en a

--- client/src/components/internship/InternshipApplicationsForStudent.jsx
+++ client/src/components/internship/InternshipApplicationsForStudent.jsx
@@ -1,9 +1,40 @@
 import React, { useEffect, useState, useContext } from "react";
+import axios from "axios";
+import CardInternshipStudent from "../shared/cards/CardInternshipStudent";
+import UserContext from "../../UserContext";
+import "./css/InternshipListStudent.css";
+import Loading from "../shared/loading/loading";
 
 function InternshipApplicationsForStudent() {
-  
-  return (
-    <div>hello</div>
-  );
+    const { userId} = useContext(UserContext);
+    const [applicationsList, setApplicationsList] = useState([]);
+    const token = localStorage.getItem("jwtToken");
+    axios.defaults.headers.common["x-access-token"] = token;
+    const URL = process.env.REACT_APP_BASE_URL;
+    const [isLoading, setIsLoading] = useState(false);
+    
+    async function getApplicationsForStudent() {
+        const response = await axios.get(
+          URL + "/api/internship/get-Applicant-List-Student",
+          {
+            studentId: userId,
+          }
+        );
+        console.log(response.data.Applicants);
+        setApplicationsList(response.data);
+      }
+
+      useEffect(() => {
+        getApplicationsForStudent();
+        console.log('lol', applicationsList);
+      }, []);
+    
+      if (isLoading) {
+        return <Loading />;
+      }
+
+    return (
+        <div>hello</div>
+    );
 }
 export default InternshipApplicationsForStudent;
--- server/routes/InternshipRoute.js
+++ server/routes/InternshipRoute.js
@@ -10,7 +10,7 @@ router.delete("/delete-internship",verifyToken,InternshipController.deleteIntern
 router.post("/update-internship",verifyToken,InternshipController.updateInternship);
 router.post("/add-Applicant",verifyToken, InternshipController.addApplicant);
 router.post("/get-Applicant-List-Employeur",InternshipController.getApplicantListEmployeur);
-router.post("/get-Applicant-List-Student",InternshipController.getApplicantListStudent);
+router.get("/get-Applicant-List-Student",InternshipController.getApplicantListStudent); //
 router.post("/set-status-Applicant",InternshipController.setStatusApplicant);
 router.post("/is-Applicant-In-List",verifyToken, InternshipController.isApplicantInList);
 router.post("/add-student",verifyToken,InternshipController.addStudent);
