6a1aa87ec89f68e52ef59c5da8b58f18de40da7f
SarahMerzouk <sarahmerzouk10@gmail.com>
2023-11-22 22:48:52 -0500

    ajout du css

--- client/src/components/internship/InternshipApplicationsForStudent.jsx
+++ client/src/components/internship/InternshipApplicationsForStudent.jsx
@@ -3,9 +3,7 @@ import axios from "axios";
 import UserContext from "../../UserContext";
 import Loading from "../shared/loading/loading";
 import { getDaysSince } from "../../utils/getDaySince";
-
-import CardInternship from "../shared/cards/CardInternshipStudent";
-import CardInternshipStudentForApplication from "../shared/cards/CardInternshioStudentForApplication";
+import "./css/internshipPage.css";
 
 function InternshipApplicationsForStudent() {
     const {userId} = useContext(UserContext);
@@ -13,8 +11,13 @@ function InternshipApplicationsForStudent() {
     axios.defaults.headers.common["x-access-token"] = token;
     const URL = process.env.REACT_APP_BASE_URL;
     const [isLoading, setIsLoading] = useState(false);
-    // let stagesInscrits = null;
     const [stagesInscrits, setStagesInscrits] = useState([]);
+    const statusTypes = {
+        'En attente': 'enAttente',
+        'Acceptée': 'accepte',
+        'Refusée' : 'refus',
+        'En révision': 'revision',
+    };
 
     async function getApplicationsForStudent() {
         try {
@@ -54,33 +57,45 @@ function InternshipApplicationsForStudent() {
         <div>
                 <div>
                     {/*<CardInternshipStudentForApplication internship={stage.internshipId}/>*/}
-                    <p>Nombre d'applications: {stagesInscrits.length}</p>
-                    <table
-                        cellpadding="0"
-                        cellspacing="0"
-                        border="0"
-                        className="applicant-table"
-                    >
+                    <h2>Liste de toutes les applications envoyées pour les stages</h2>
+                    <hr />
 
-                            <tr>
-                                <th>Titre</th>
-                                <th>Compagnie</th>
-                                <th>Date d'application</th>
-                                <th>Personne contact</th>
-                                <th>Statue de la demande</th>
-                            </tr>
+                    {stagesInscrits && stagesInscrits.length > 0 && (
+                        <div>
+                            <h4>Nombre d'applications: {stagesInscrits.length}</h4>
+                            <table
+                                cellpadding="0"
+                                cellspacing="0"
+                                border="0"
+                                className="applicant-table"
+                            >
+                                    <tr className="applicant-titre-table-row">
+                                        <th className="applicant-titre-table-colum">Titre</th>
+                                        <th className="applicant-titre-table-colum">Compagnie</th>
+                                        <th className="applicant-titre-table-colum">Date d'application</th>
+                                        <th className="applicant-titre-table-colum">Personne contact</th>
+                                        <th className="applicant-titre-table-colum">Statue de la demande</th>
+                                    </tr>
 
-                        {stagesInscrits && stagesInscrits.map((stage) => (
-                           <tr>
-                                <td>{stage?.internshipId.internshiptitle}</td>
-                                <td>{stage?.internshipId.companyname}</td>
-                                <td>{getDaysSince(stage.internshipId.creationdate)}</td>
-                                <td>{stage?.internshipId.contactemail}</td>
-                                <td>{stage?.status}</td>
-                           </tr>
-                        ))}
-
-                    </table>
+                                {stagesInscrits && stagesInscrits.map((stage) => (
+                                <tr
+                                        key={stage._id}
+                                        className="applicant-table-row"
+                                    >
+                                        <td className="applicant-table-colum">{stage?.internshipId.internshiptitle}</td>
+                                        <td className="applicant-table-colum">{stage?.internshipId.companyname}</td>
+                                        <td className="applicant-table-colum">{getDaysSince(stage.internshipId.creationdate)}</td>
+                                        <td className="applicant-table-colum">{stage?.internshipId.contactemail}</td>
+                                        <td className={statusTypes[stage?.status]}>{stage?.status}</td>                                     
+                                </tr>
+                                ))}
+                            </table>
+                        </div>
+                    )}
+                
+                    {stagesInscrits.length === 0 && 
+                        <p className="noSoumission">Aucune application</p>
+                    }
                 </div>
     
         </div>
