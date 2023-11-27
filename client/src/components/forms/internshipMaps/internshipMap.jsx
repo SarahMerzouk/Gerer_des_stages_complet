dfe1c78e1eb2dcbf862b99d19e9b9fae98c26f6a
EricMarinMejia <ericmarin14@hotmail.com>
2023-11-27 17:48:06 -0500

    Error with CORS

---
+++ client/src/components/forms/internshipMaps/internshipMap.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router";
+import axios from "axios";
+
+
+{/*
+1. Verify adress https://developers.google.com/maps/documentation/address-validation/overview?hl=fr (show error is )
+2. Turn it into geolocation with latitude and longitude https://developers.google.com/maps/documentation/geocoding/overview?hl=fr
+3. Show map https://cloud.google.com/blog/products/maps-platform/introducing-react-components-for-the-maps-javascript-api
+*/}
+
+function InternshipMap() {
+    const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+    const location = useLocation();
+    const internship = location.state;
+    const adresse = internship.companyadresse;
+    const [adresseValide, setAdresseValide] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const validateAddress = async () => {
+            try {
+                const response = await axios.get(
+                    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(adresse)}&key=${API_KEY}`
+                );
+
+                console.log(response)
+
+                if (response.data.status === "OK" && (response.data.results[0].geometry.location_type === "ROOFTOP")) {
+                    setAdresseValide(true);
+                } else {
+                    setAdresseValide(false);
+                }
+
+                console.log(response)
+
+                setLoading(false); // Set loading to false once the validation is complete
+            } catch (error) {
+                console.error('Error validating address:', error);
+                setAdresseValide(false);
+                setLoading(false); // Set loading to false in case of an error
+            }
+        };
+
+        validateAddress();
+    }, [adresse, API_KEY]);
+
+    // Conditional rendering based on the value of adresseValide
+    return (
+        <div>
+            {loading && <p>Loading...</p>}
+            {!loading && (
+                <>
+                    {adresseValide ? (
+                        <div>
+                            <p>Adresse Valide</p>
+                            <p>{adresse}</p>
+                        </div>
+                    ) : (
+                        <div>
+                            <p>Impossible de trouver l'adresse sur la Carte</p>
+                            <p>{adresse}</p>
+                        </div>
+                    )}
+                </>
+            )}
+        </div>
+    );
+}
+
+export default InternshipMap;
